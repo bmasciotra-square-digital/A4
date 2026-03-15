87763ab5016f6537d1abcd76735e8842b504c94d
bmasciotra-square-digital <brad@squaredigital.com>
2026-03-14 19:50:21 -0700

    updated files

--- main.py
+++ main.py
@@ -5,13 +5,8 @@ from task_four.task_four import task_four
 from task_five.task_five import task_five
 
 if __name__ == '__main__':
-    # Task One
     rsa = task_one()
-
-    # Task Two
     aes = task_two()
-
-    # Task Three
     task_three(rsa)
     task_four(aes)
     task_five(rsa)
--- task_five/task_five.py
+++ task_five/task_five.py
@@ -12,12 +12,26 @@ def task_five(rsa: RSA):
     # CRT-optimized RSA decryption in your key exchange.
 
     message = "Cryptology is cool"
+    long_message = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of ' deFinibusBonorum et Malorum'(The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, 'Loremipsum dolorsitamet.., comes from a line in section 1.10.32.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from ' de Finibus Bonorum etMalorum' by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'"
     cipher = rsa.encrypt(message)
+    long_cipher = rsa.encrypt(long_message)
 
     start = time.perf_counter()
     plaintext_crt = rsa.decrypt_crt(cipher)
     end = time.perf_counter()
+
     print("RSA CRT Decryption Time:", end - start, "seconds")
 
+    start = time.perf_counter()
+    long_plaintext_crt = rsa.decrypt_crt(long_cipher)
+    end = time.perf_counter()
+
+    print("Long CRT Decryption Time:", end - start, "seconds")
+
+    start = time.perf_counter()
+    long_plaintext_crt = rsa.decrypt(long_cipher)
+    end = time.perf_counter()
+
+    print("Long Decryption Time:", end - start, "seconds")
+
     print(f"message: \"{message}\" | cipher: {cipher} | plaintext: {plaintext_crt} | {message == plaintext_crt}")
-    pass
